25145ffe9551a590bb43c1996a4ab6abdcf81b0b
Daniel Freire <dgz1606@gmail.com>
2024-02-19 17:36:15 -0500

    add custom enum to handle error messages and minor button fix

--- GHFollowers/Screens/FollowerListViewController.swift
+++ GHFollowers/Screens/FollowerListViewController.swift
@@ -14,18 +14,22 @@ class FollowerListViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-        
+
         NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
             guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: errorMessage!, buttonTitle: "Ok")
+                self.presentGFAlertOnMainThread(
+                    title: "Bad Stuff Happend",
+                    message: errorMessage!.rawValue,
+                    buttonTitle: "Ok"
+                )
                 return
             }
-            
+
             print("Followers count = \(followers.count)")
             print(followers)
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
