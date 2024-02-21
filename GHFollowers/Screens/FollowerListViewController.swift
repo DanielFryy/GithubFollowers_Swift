acff1be6a5f26372172c7c0717238c94a7f7af7a
Daniel Freire <dgz1606@gmail.com>
2024-02-21 18:23:13 -0500

    fix potential memory leak in network call and create a helper for UI

--- GHFollowers/Screens/FollowerListViewController.swift
+++ GHFollowers/Screens/FollowerListViewController.swift
@@ -8,9 +8,7 @@
 import UIKit
 
 class FollowerListViewController: UIViewController {
-    enum Section {
-        case main
-    }
+    enum Section { case main }
 
     var username: String!
     var followers = [Follower]()
@@ -37,7 +35,10 @@ class FollowerListViewController: UIViewController {
     }
 
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(
+            frame: view.bounds,
+            collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view)
+        )
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
         collectionView.register(
@@ -46,22 +47,10 @@ class FollowerListViewController: UIViewController {
         )
     }
 
-    func createThreeColumnFlowLayout() -> UICollectionViewLayout {
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidht = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth = availableWidht / 3
-
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-
-        return flowLayout
-    }
-
     func getFollowers() {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+            guard let self = self else { return }
+
             switch result {
             case let .success(followers):
                 self.followers = followers
