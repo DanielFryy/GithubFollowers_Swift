71b13e6d79fb3bf6299fd7ac796031f337c8ef45
Daniel Freire <dgz1606@gmail.com>
2024-02-21 19:00:29 -0500

    download avatar image and cache them

--- GHFollowers/Custom Views/Cells/FollowerCollectionViewCell.swift
+++ GHFollowers/Custom Views/Cells/FollowerCollectionViewCell.swift
@@ -11,25 +11,27 @@ class FollowerCollectionViewCell: UICollectionViewCell {
     static let reuseID = "FollowerCollectionViewCell"
     let avatarImageView = GFAvatarImageView(frame: .zero)
     let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
     }
-    
-    required init?(coder: NSCoder) {
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func set(follower: Follower) {
         usernameLabel.text = follower.login
+        avatarImageView.downloadImage(from: follower.avatarUrl)
     }
-    
+
     private func configure() {
         addSubview(avatarImageView)
         addSubview(usernameLabel)
         let padding: CGFloat = 8
-        
+
         NSLayoutConstraint.activate([
             avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: padding),
             avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
@@ -38,7 +40,7 @@ class FollowerCollectionViewCell: UICollectionViewCell {
             usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
             usernameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
             usernameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
-            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20),
         ])
     }
 }
--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Daniel Freire on 2/18/24.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users"
+    private let baseURL = "https://api.github.com/users"
+    let cache = NSCache<NSString, UIImage>()
 
     private init() {}
 
