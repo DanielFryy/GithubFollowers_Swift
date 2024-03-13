fc458bf3306a8ffaba12a4c2497f5d8073a6d52d
Daniel Freire <dgz1606@gmail.com>
2024-03-13 18:08:09 -0500

    add scroll view to user info screen

--- GHFollowers/Extensions/UIView+Ext.swift
+++ GHFollowers/Extensions/UIView+Ext.swift
@@ -8,9 +8,36 @@
 import UIKit
 
 extension UIView {
+    func pinToEdges(of superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superView.topAnchor),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor)
+        ])
+    }
+
     func addSubviews(_ views: UIView...) {
         for view in views {
             addSubview(view)
         }
+    }
+
+    func centerX(in superView: UIView) {
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(centerXAnchor.constraint(equalTo: superView.centerXAnchor))
+        NSLayoutConstraint.activate(constraints)
+    }
+
+    func centerY(in superView: UIView) {
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(centerYAnchor.constraint(equalTo: superView.centerYAnchor))
+        NSLayoutConstraint.activate(constraints)
+    }
+
+    func center(in superView: UIView) {
+        centerX(in: superView)
+        centerY(in: superView)
     }
 }
